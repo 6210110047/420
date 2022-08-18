9ca58111b7cf34d9a8d7ac5c7fadc89eb249b46b
uekiz <kingghostdragon@gmail.com>
2022-08-18 13:05:23 +0700

    Updates

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-const React =require('react')
-const Layout = require('./src/components/layout').default
-
-exports.wrapPageElement = ({props, element}) =>{
-    return <Layout {...props}>
-        {element}
-    </Layout>
-}
